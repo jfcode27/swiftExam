a17fb096d826c3c2e60cf6c25e57be28cd3208ed
Jeremy Franco <50761613+jfcode27@users.noreply.github.com>
2022-05-06 16:31:41 -0500

    Initial Commit

---
+++ Examen_2P/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen_2P
+//
+//  Created by Jeremy Franco on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
